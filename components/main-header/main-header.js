b84c380efdae0d34f5bc09929b6829677cc5271b
Govind Dandekar <govind.dandekar@gmail.com>
2025-01-20 11:30:56 -0800

    create NavLinks and render on client for usePathname hook

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -1,7 +1,8 @@
-import Link from 'next/link';
 import Image from 'next/image';
 
 import MainHeaderBackground from './main-header-background';
+import NavLink from './nav-link';
+import Link from 'next/link';
 
 // next Image component automatically detects image size and other properties
 // and will automatically serve image in optimized format
@@ -20,10 +21,18 @@ function MainHeader(){
 		<nav className={classes.nav}>
 			<ul>
 				<li>
-					<Link href="/meals">Browse Meals</Link>
+					<NavLink
+						href='/meals'
+					>
+						Browse Meals
+					</NavLink>
 				</li>
 				<li>
-					<Link href="/community">Foodies Community</Link>
+				<NavLink
+						href='/community'
+					>
+						Foodies Community
+				</NavLink>
 				</li>
 			</ul>
 		</nav>

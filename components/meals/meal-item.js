3bf82810fc48d5824de3ae9cf5c4ceac30b83b07
Govind Dandekar <govind.dandekar@gmail.com>
2025-01-20 11:55:35 -0800

    add MealItem and MealsGrid components

---
+++ components/meals/meal-item.js
@@ -0,0 +1,29 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+import classes from './meal-item.module.css';
+
+export default function MealItem({ title, slug, image, summary, creator }) {
+  return (
+    <article className={classes.meal}>
+      <header>
+        <div className={classes.image}>
+					{/* meal item images to be loaded dynamically from DB */}
+					{/* image width and height not available at build time */}
+					{/* fill prop added to account for lack of width and height info at build time */}
+          <Image src={image} alt={title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h2>{title}</h2>
+          <p>by {creator}</p>
+        </div>
+      </header>
+      <div className={classes.content}>
+        <p className={classes.summary}>{summary}</p>
+        <div className={classes.actions}>
+          <Link href={`/meals/${slug}`}>View Details</Link>
+        </div>
+      </div>
+    </article>
+  );
+}

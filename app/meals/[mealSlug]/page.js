e925a5b75cf35f710a1254687bc733169b62e827
Govind Dandekar <govind.dandekar@gmail.com>
2025-01-21 10:23:20 -0800

    implement MealDetailsPage

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,8 +1,51 @@
-function MealDetailsPage({params}){
-	
-	return <main>
-		<h1>Meal Details</h1>
-	</main>
+import Image from 'next/image';
+import { notFound } from 'next/navigation';
+
+import { getMeal } from '@/lib/meals'; 
+
+import classes from './page.module.css';
+
+// evenry component stored in page.js gets params
+// [slugName]: url 
+function MealDetailsPage({ params }){
+
+	const meal = getMeal(params.mealSlug);
+
+	// if no meal found (bad url)
+	if (!meal){
+		// stops component from executing and will
+		// show closest not found || error page
+		notFound();
+	}
+
+	// regex to replace newline with line break
+	meal.instructions = meal.instructions.replace(/\n/g, '<br />')
+
+	return <>
+		<header className={classes.header}>
+			<div className={classes.image}>
+				<Image src={meal.image} alt={meal.title} fill />
+			</div>
+			<div className={classes.headerText}>
+				<h1>{meal.title}</h1>
+				<p className={classes.creator}>
+					by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+				</p>
+				<p className={classes.summary}>
+					{meal.summary}
+				</p>
+			</div>
+		</header>
+		<main>
+			<p 
+				className={classes.instructions}
+				dangerouslySetInnerHTML= {{
+					__html: meal.instructions
+				}}	
+			>
+			</p>
+		</main>
+	</>
 }
 
 export default MealDetailsPage;
